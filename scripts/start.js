e8b45f2c96900dd3b39fe6ade5bd1af4b84a5dbb
Anatoliy <tolxpams@gmail.com>
2022-01-10 13:38:29 +0200

    rhombus

--- scripts/start.js
+++ scripts/start.js
@@ -1,7 +1,15 @@
 window.onload = function() {
+  //change 2 and 6 block
   let block2 = document.querySelector("header");
   let block6 = document.querySelector("footer");
   let tmp = block2.innerHTML;
   block2.innerHTML = block6.innerHTML;
   block6.innerHTML = tmp;
+
+  //add to 5 rhombus area
+  const a = 5;
+  const b = 9;
+  let s = a*b/2
+  let block5 = document.querySelector("article");
+  block5.innerHTML += `<p>The area of the rhombus is ${s}</p>`
 }
